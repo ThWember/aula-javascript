59046e56d8e05a13e33c56f66058d922e195af5e
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-12 11:46:34 -0300

    Updated "clicou" function

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ var name = "Thais Wemberlaine Dias Machado Rocha";
 alert ("Página desenvolvida por " + name);
 
 function clicou() {
-    alert("Obrigada por clicar")
+    alert("Obrigada por clicar");
+    document.getElementById("boas-vindas").innerHTML = "Bem-vinda à minha página";
 }
