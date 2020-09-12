67937c6dbf55ad0a5103e033e8903ad3b3fec1b9
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-12 12:46:23 -0300

    Updated function "redirecionar"

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ function clicou() {
     alert("Obrigada por clicar");
     document.getElementById("boas-vindas").innerHTML = "Bem-vinda à minha página";
     document.getElementById("github").innerHTML = "<b>Github</b>";
+    document.getElementById("redirecionar");
+}
+
+function trocar() {
+    document.getElementById("redirecionar").innerHTML = "<b>Ir para a minha página no github</b>";
 }
 
 function redirecionar() {
     window.open("https://github.com/ThWember/");
 }
+
