0c144bf1c484d3ccef68d7b056267013b1128756
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-12 11:58:13 -0300

    Updated functions

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ alert ("Página desenvolvida por " + name);
 function clicou() {
     alert("Obrigada por clicar");
     document.getElementById("boas-vindas").innerHTML = "Bem-vinda à minha página";
+    document.getElementById("github").innerHTML = "<b>Github</b>";
+}
+
+function redirecionar() {
+    window.open("https://github.com/ThWember/");
 }
